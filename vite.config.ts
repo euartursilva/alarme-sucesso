577db1e2a791d33f93c09f6624c2d88f874be171
Artur Silva Araújo <102360806+euartursilva@users.noreply.github.com>
2026-09-19 00:52:34 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/alarme-sucesso/',
+  plugins: [react()],
+})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
   plugins: [react()],
 })
