d162dd5100f940c1c9f0fb3bedfae962f3aefd98
Artur Silva Araújo <102360806+euartursilva@users.noreply.github.com>
2026-09-19 00:53:42 -0300

    Restore base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,3 @@ export default defineConfig({
   base: '/alarme-sucesso/',
   plugins: [react()],
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
